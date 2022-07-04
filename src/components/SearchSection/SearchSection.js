c94631f766c6ef49a3f364b511be6ca094bb6869
Manuel Leung Chen <manuel.leungchen@gmail.com>
2022-07-03 21:24:01 -0400

    Fixed major accessability issues

--- src/components/SearchSection/SearchSection.js
+++ src/components/SearchSection/SearchSection.js
@@ -54,8 +54,10 @@ function SearchSection() {
             <h2>Find the best time for a subreddit</h2>
 
             <form id='form' role={"search"} className={styles.searchbar} onSubmit={handleFormSubmit}>
-                <input type="search" name='q' placeholder='Enter a subreddit' aria-label='Search and analyze a subreddit' className={styles.searchbarInput} value={subredditSearch} onChange={handleInputChange} />
+                <span>r/</span>
+                <input type="search" name='q' placeholder='Enter a subreddit' aria-label='Search and analyze a subreddit' className={styles.searchbarInput} value={subredditSearch} onChange={handleInputChange} ></input>
                 <button className={styles.button} onClick={handleFormSubmit}>
+                    <span className='sr-only'>Search</span>
                     <svg viewBox="0 0 1024 1024"><path className="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
                 </button>
             </form>
@@ -70,7 +72,7 @@ function SearchSection() {
                     <p><strong>Danger!</strong> The subreddit &ldquo;{subredditSearch}&rdquo; doesnt exist. Please try another subreddit.</p>
                 </div>
             ) : (
-                <>                    
+                <>
                     {/* If device width is greater than 920 px, show desktop version of heatmap. Otherwise show mobile heatmap */}
                     {deviceSize.width > 920 ? <Heatmap data={data} /> : <HeatmapMobile data={data} />}
 
