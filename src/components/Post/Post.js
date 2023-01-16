56346fa5294567c42c5cfbb4c817f959beab75fd
Manuel Leung Chen <manuel.leungchen@gmail.com>
2023-01-15 21:01:12 -0500

    Fixed posts list bug

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -8,14 +8,14 @@ function Post({ title, score, commentsCount, created, link, deviceSize }) {
         <>
             {deviceSize.width > 920 ?
             <tr>
-                <td><a className={styles.postLink} href={`https://www.reddit.com${link}`} target="_blank" >{title}<span className="sr-only">(Open in new window)</span></a></td>
-                <td> {dateTimeInSeconds.toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric'})} <br /> {dateTimeInSeconds.toLocaleTimeString(undefined, { hour: 'numeric', minute: 'numeric'})}</td>
-                <td>{score}</td>
-                <td>{commentsCount}</td>
+                <td className={styles.tbody}><a className={styles.postLink} href={`https://www.reddit.com${link}`} target="_blank" >{title}<span className="sr-only">(Open in new window)</span></a></td>
+                <td className={styles.tbody}>{dateTimeInSeconds.toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric'})} <br /> {dateTimeInSeconds.toLocaleTimeString(undefined, { hour: 'numeric', minute: 'numeric'})}</td>
+                <td className={styles.tbody}>{score}</td>
+                <td className={styles.tbody}>{commentsCount}</td>
             </tr>
             :
                 <div className={styles.post}>
-                    <a className={styles.postLink} href={`https://www.reddit.com${link}`} target="_blank">{title}</a>
+                    <div className={styles.postLinkDiv}><a className={styles.postLink} href={`https://www.reddit.com${link}`} target="_blank">{title}</a></div>
                     <div className={styles.postDetails}>
                         <span>{dateTimeInSeconds.toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric' })} {dateTimeInSeconds.toLocaleTimeString(undefined, { hour: 'numeric', minute: 'numeric' })}</span>
                         <span>{score} votes</span>
